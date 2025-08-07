75bdf566a1d44cb11476097c6f33b36f928c45a1
vophuctien30102003 <vophuctien3010@gmail.com>
2025-08-07 13:26:24 +0700

    Setup:  base project #1

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Background } from "@react-navigation/elements";
+import { Text } from "react-native";
+
+export default function Index() {
+    return (
+        <Background>
+            <Text className="text-lg font-bold text-primary">
+                Edit app/index.tsx to edit this screen. xin chào bạn 👋
+            </Text>
+        </Background>
+    );
+}

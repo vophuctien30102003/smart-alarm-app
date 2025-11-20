4cb6a4de73084016d20258854b8752b29260a24b
vophuctien30102003 <vophuctien3010@gmail.com>
2025-11-20 15:17:32 +0700

    clean code alarm clock

--- src/app/clock/index.tsx
+++ src/app/clock/index.tsx
@@ -1,9 +1,5 @@
-import { AlarmTabContent } from "@/components/features/alarm-clock/components/AlarmTabContent";
+import AlarmTabContent from "@/components/features/alarm-clock/components/AlarmTabContent";
 import HeaderTabs from "@/components/features/alarm-clock/components/HeaderTabClock";
-import SetAlarmScreen from "@/components/features/alarm-clock/screens/SetAlarmClockScreen";
-import { useSleepAlarmManagement } from "@/hooks/useSleepAlarmManagement";
-import type { SleepAlarmFormData } from "@/shared/types/sleepAlarmForm.type";
-import { formatTime } from "@/shared/utils/timeUtils";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 import { SafeAreaView, Text, View } from "react-native";
@@ -11,59 +7,14 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function AlarmClockPage() {
     const [activeTab, setActiveTab] = useState<"alarm" | "timer">("alarm");
-    const [sleepGoalMinutes, setSleepGoalMinutes] = useState(8 * 60);
     const insets = useSafeAreaInsets();
-    const {
-        sleepAlarms,
-        showSetAlarm,
-        handleSaveAlarm,
-        handleBackFromSetAlarm,
-        startAddAlarm,
-        startEditAlarm,
-        getEditingAlarmData,
-    } = useSleepAlarmManagement();
-
-    const buildDefaultSleepAlarmData = (): Partial<SleepAlarmFormData> => {
-        const now = new Date();
-        const bedtime = formatTime(now);
-        const wakeTime = formatTime(
-            new Date(now.getTime() + sleepGoalMinutes * 60 * 1000),
-        );
-
-        return {
-            bedtime,
-            wakeTime,
-            goalMinutes: sleepGoalMinutes,
-        };
-    };
-
-    if (showSetAlarm) {
-        const initialData = getEditingAlarmData() ?? buildDefaultSleepAlarmData();
-
-        return (
-            <SafeAreaView className="flex-1">
-                <SetAlarmScreen
-                    initialData={initialData}
-                    onSave={handleSaveAlarm}
-                    onBack={handleBackFromSetAlarm}
-                />
-            </SafeAreaView>
-        );
-    }
-
     return (
         <SafeAreaView className="flex-1" >
             <LinearGradient colors={["#9887C3", "#090212"]} style={{ paddingTop: insets.top }}  className="flex-1 ">
                 <HeaderTabs activeTab={activeTab} onChangeTab={setActiveTab} />
 
                 {activeTab === "alarm" ? (
-                    <AlarmTabContent
-                        sleepGoalMinutes={sleepGoalMinutes}
-                        onSleepGoalChange={setSleepGoalMinutes}
-                        sleepAlarms={sleepAlarms}
-                        onAddNewAlarm={startAddAlarm}
-                        onEditAlarm={startEditAlarm}
-                    />
+                    <AlarmTabContent/>
                 ) : (
                     <View className="flex-1 items-center justify-center">
                         <Text className="text-white text-lg">
--- src/components/features/alarm-clock/components/AlarmTabContent.tsx
+++ src/components/features/alarm-clock/components/AlarmTabContent.tsx
@@ -1,39 +1,39 @@
-import type { SleepAlarm } from "@/shared/types/alarm.type";
 import { Text, View } from "react-native";
-import { AlarmListSection } from "./AlarmListSection";
 import SleepGoalSelector from "./SleepGoalSelector";
+import { useState } from "react";
+import { useSleepAlarmManagement } from "@/hooks/useSleepAlarmManagement";
+import ListAlarmClock from "./ListAlarmClock";
 
-interface AlarmTabContentProps {
-    sleepGoalMinutes: number;
-    onSleepGoalChange: (minutes: number) => void;
-    sleepAlarms: SleepAlarm[];
-    onAddNewAlarm: () => void;
-    onEditAlarm: (alarm: SleepAlarm) => void;
+export default function AlarmTabContent() {
+    const [sleepGoalMinutes, setSleepGoalMinutes] = useState(8 * 60);
+    const { sleepAlarms, startAddAlarm, startEditAlarm } =
+        useSleepAlarmManagement();
+
+    return (
+        <View className="flex-1 px-6 pt-4 ">
+            <Text className="text-[#F8FAFC] text-[22px] font-bold text-center mb-2">
+                Set your sleep schedule
+            </Text>
+            <Text className="text-[#B3B3B3] text-center mb-8 ">
+                Your schedule is used to send bedtime reminders and suggest
+                wake-up alerts.
+            </Text>
+            <SleepGoalSelector
+                sleepGoalMinutes={sleepGoalMinutes}
+                onChangeSleepGoal={setSleepGoalMinutes}
+            />
+            <>
+                <View>
+                    <Text className="text-white text-xl font-semibold mb-4">
+                        Your Sleep Schedule
+                    </Text>
+                </View>
+                <ListAlarmClock
+                    alarms={sleepAlarms}
+                    onEditAlarm={startEditAlarm}
+                    onAddNewAlarm={startAddAlarm}
+                />
+            </>
+        </View>
+    );
 }
-
-export const AlarmTabContent: React.FC<AlarmTabContentProps> = ({
-    sleepGoalMinutes,
-    onSleepGoalChange,
-    sleepAlarms,
-    onAddNewAlarm,
-    onEditAlarm,
-}) => (
-    <View className="flex-1 px-6 pt-4 ">
-        <Text className="text-[#F8FAFC] text-[22px] font-bold text-center mb-2">
-            Set your sleep schedule
-        </Text>
-        <Text className="text-[#B3B3B3] text-center mb-8 ">
-            Your schedule is used to send bedtime reminders and suggest
-            wake-up alerts.
-        </Text>
-        <SleepGoalSelector
-            sleepGoalMinutes={sleepGoalMinutes}
-            onChangeSleepGoal={onSleepGoalChange}
-        />
-        <AlarmListSection
-            sleepAlarms={sleepAlarms}
-            onEditAlarm={onEditAlarm}
-            onAddNewAlarm={onAddNewAlarm}
-        />
-    </View>
-);
